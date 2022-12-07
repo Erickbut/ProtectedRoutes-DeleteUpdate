128ec60fe503f0039766a7a447b667aafe1106a2
Erickbut <erickbutron1@gmail.com>
2022-12-06 20:06:43 -0400

    add protected Routes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const db = require('./utils/database')
 const port = require('../config').api.port
 
 const userRouter = require('./users/users.router')
+const authRouter = require('./auth/auth.router')
+const passport = require('passport')
+
 
 db.authenticate()
     .then(() => console.log('Database Authenticated'))
@@ -16,11 +19,12 @@ db.sync()
 
 app.use(express.json())
 
-app.get('/',  (req, res) => {
-    res.status(200).json({message: 'Ok!'})
+app.get('/', passport.authenticate('jwt',{session:false}),  (req, res) => {
+    res.status(200).json({message: `Bienvenido! ${req.user.user_name}`})
 })
 
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/auth', authRouter)
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
